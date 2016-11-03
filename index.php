9bfb388788e58d09637c16c7a27f764b7ca25198
Roy Tanck <roy.tanck@gmail.com>
2016-11-03 20:29:36 +0100

    * Removed potentially copyrighted images. * Added photo credits and some new images. * Reworked the CSS in index.php a bit. * Moved the failed js experiment to a folder to avoid confusion.

--- index.php
+++ index.php
@@ -17,92 +17,57 @@
 				font-size: 0px;
 			}
 			.block img {
-				margin: 0 20px 20px 0;
+				height: 250px;
+				width: auto;
+				margin: 0 20px 5px 0;
 				border: 1px solid rgba(255,255,255,0.5);				
+			}
+			p.credit {
+				font-size: 0.75em;
+				margin: 0 0 30px 0;
+			}
+			a {
+				color: #fff !important;
 			}
 		</style>
 	</head>
 	<body>
 
-	<p id="source" class="block">
-		<img src="autocrop.php?img=images/image1.jpg&w=250&h=250&demo=true" alt="cropped image" style="height:250px; width: auto;" />
-		<img src="autocrop.php?img=images/image1.jpg&w=250&h=250" alt="cropped image" />
-	</p>
+		<p id="source" class="block">
+			<img src="autocrop.php?img=images/image1.jpg&w=250&h=250&demo=true" alt="cropped image" class="original" />
+			<img src="autocrop.php?img=images/image1.jpg&w=250&h=250" alt="cropped image" />
+			<p class="credit">Photo by <a href="https://unsplash.com/@dsmacinnes">Danielle MacInnes</a></p>
+		</p>
 
-	<p id="source" class="block">
-		<img src="autocrop.php?img=images/image3.jpg&w=250&h=250&demo=true" alt="cropped image" style="height:250px; width: auto;" />
-		<img src="autocrop.php?img=images/image3.jpg&w=250&h=250" alt="cropped image" />
-	</p>
+		<p id="source" class="block">
+			<img src="autocrop.php?img=images/image2.jpg&w=250&h=250&demo=true" alt="cropped image" class="original" />
+			<img src="autocrop.php?img=images/image2.jpg&w=250&h=250" alt="cropped image" />
+			<p class="credit">Photo by <a href="https://unsplash.com/@chrisjoelcampbell">Christopher Campbell</a></p>
+		</p>
 
-	<p id="source" class="block">
-		<img src="autocrop.php?img=images/image4.jpg&w=250&h=250&demo=true" alt="cropped image" style="height:250px; width: auto;" />
-		<img src="autocrop.php?img=images/image4.jpg&w=250&h=250" alt="cropped image" />
-	</p>
+		<p id="source" class="block">
+			<img src="autocrop.php?img=images/image3.jpg&w=250&h=250&demo=true" alt="cropped image" class="original" />
+			<img src="autocrop.php?img=images/image3.jpg&w=250&h=250" alt="cropped image" />
+			<p class="credit">Photo by <a href="https://unsplash.com/@spiritvisionstudios">Ben Rosett</a></p>
+		</p>
 
-	<p id="source" class="block">
-		<img src="autocrop.php?img=images/image5.jpg&w=250&h=250&demo=true" alt="cropped image" style="height:250px; width: auto;" />
-		<img src="autocrop.php?img=images/image5.jpg&w=250&h=250" alt="cropped image" />
-	</p>
+		<p id="source" class="block">
+			<img src="autocrop.php?img=images/image4.jpg&w=250&h=250&demo=true" alt="cropped image" class="original" />
+			<img src="autocrop.php?img=images/image4.jpg&w=250&h=250" alt="cropped image" />
+			<p class="credit">Photo by <a href="https://unsplash.com/@wexor">Wexor Tmg</a></p>
+		</p>
 
-	<p id="source" class="block">
-		<img src="autocrop.php?img=images/image6.jpg&w=250&h=250&demo=true" alt="cropped image" style="height:250px; width: auto;" />
-		<img src="autocrop.php?img=images/image6.jpg&w=250&h=250" alt="cropped image" />
-	</p>
+		<p id="source" class="block">
+			<img src="autocrop.php?img=images/image5.jpg&w=250&h=250&demo=true" alt="cropped image" class="original" />
+			<img src="autocrop.php?img=images/image5.jpg&w=250&h=250" alt="cropped image" />
+			<p class="credit">Photo by <a href="https://unsplash.com/@jeffkingla">Jeff King</a></p>
+		</p>
 
-	<p id="source" class="block">
-		<img src="autocrop.php?img=images/image7.jpg&w=250&h=250&demo=true" alt="cropped image" style="height:250px; width: auto;" />
-		<img src="autocrop.php?img=images/image7.jpg&w=250&h=250" alt="cropped image" />
-	</p>
-
-	<p id="source" class="block">
-		<img src="autocrop.php?img=images/image8.jpg&w=250&h=250&demo=true" alt="cropped image" style="height:250px; width: auto;" />
-		<img src="autocrop.php?img=images/image8.jpg&w=250&h=250" alt="cropped image" />
-	</p>
-
-	<p id="source" class="block">
-		<img src="autocrop.php?img=images/image9.jpg&w=250&h=250&demo=true" alt="cropped image" style="height:250px; width: auto;" />
-		<img src="autocrop.php?img=images/image9.jpg&w=250&h=250" alt="cropped image" />
-	</p>
-
-	<p id="source" class="block">
-		<img src="autocrop.php?img=images/image10.jpg&w=250&h=250&demo=true" alt="cropped image" style="height:250px; width: auto;" />
-		<img src="autocrop.php?img=images/image10.jpg&w=250&h=250" alt="cropped image" />
-	</p>
-
-	<p id="source" class="block">
-		<img src="autocrop.php?img=images/image11.jpg&w=250&h=250&demo=true" alt="cropped image" style="height:250px; width: auto;" />
-		<img src="autocrop.php?img=images/image11.jpg&w=250&h=250" alt="cropped image" />
-	</p>
-
-	<p id="source" class="block">
-		<img src="autocrop.php?img=images/image12.jpg&w=250&h=250&demo=true" alt="cropped image" style="height:250px; width: auto;" />
-		<img src="autocrop.php?img=images/image12.jpg&w=250&h=250" alt="cropped image" />
-	</p>
-
-	<p id="source" class="block">
-		<img src="autocrop.php?img=images/image13.jpg&w=250&h=250&demo=true" alt="cropped image" style="height:250px; width: auto;" />
-		<img src="autocrop.php?img=images/image13.jpg&w=250&h=250" alt="cropped image" />
-	</p>
-
-	<p id="source" class="block">
-		<img src="autocrop.php?img=images/image14.jpg&w=250&h=250&demo=true" alt="cropped image" style="height:250px; width: auto;" />
-		<img src="autocrop.php?img=images/image14.jpg&w=250&h=250" alt="cropped image" />
-	</p>
-
-	<p id="source" class="block">
-		<img src="autocrop.php?img=images/image15.jpg&w=250&h=250&demo=true" alt="cropped image" style="height:250px; width: auto;" />
-		<img src="autocrop.php?img=images/image15.jpg&w=250&h=250" alt="cropped image" />
-	</p>
-
-	<p id="source" class="block">
-		<img src="autocrop.php?img=images/image16.jpg&w=250&h=250&demo=true" alt="cropped image" style="height:250px; width: auto;" />
-		<img src="autocrop.php?img=images/image16.jpg&w=250&h=250" alt="cropped image" />
-	</p>
-
-	<p id="source" class="block">
-		<img src="autocrop.php?img=images/image17.jpg&w=250&h=250&demo=true" alt="cropped image" style="height:250px; width: auto;" />
-		<img src="autocrop.php?img=images/image17.jpg&w=250&h=250" alt="cropped image" />
-	</p>
+		<p id="source" class="block">
+			<img src="autocrop.php?img=images/image6.jpg&w=250&h=250&demo=true" alt="cropped image" class="original" />
+			<img src="autocrop.php?img=images/image6.jpg&w=250&h=250" alt="cropped image" />
+			<p class="credit">Photo by <a href="https://unsplash.com/@rickpsd">Henrique Ferreira</a></p>
+		</p>
 
 	</body>
 </html>
